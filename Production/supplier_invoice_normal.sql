3e47511e02bedd9f5b53879b2dcc0f57de78f0cc
Rizki H <rizki.habibie@tiket.com>
2021-08-03 16:44:57 +0700

    Update SI Normal

--- Production/supplier_invoice_normal.sql
+++ Production/supplier_invoice_normal.sql
@@ -5,7 +5,7 @@ lsw as (
     , order_detail_id
     , 1 as is_ci_sent_flag
   from
-    `datamart-finance.datasource_workday.log_sent_to_workday`
+    `datamart-finance.datamart_edp.log_sent_to_workday`
   where
     calculation_type_name = 'customer_invoice'
     and status_name = 'normal_order'
@@ -20,7 +20,7 @@ lsw as (
     , order_detail_id
     , 1 as is_supplier_invoice_sent_flag
   from
-    `datamart-finance.datasource_workday.log_sent_to_workday`
+    `datamart-finance.datamart_edp.log_sent_to_workday`
   where
     calculation_type_name = 'supplier_invoice'
     and date(created_timestamp) >= date_add(date(current_timestamp(),'Asia/Jakarta'), interval -3 day)
@@ -36,33 +36,45 @@ lsw as (
         *
         , row_number() over(partition by order_id, order_detail_id, spend_category order by processed_timestamp desc) as rn
       from
-        `datamart-finance.datasource_workday.supplier_invoice_raw`
+        `datamart-finance.datamart_edp.supplier_invoice_raw_OTW`
       where date(invoice_date) >= date_add(date(current_timestamp(),'Asia/Jakarta'), interval -3 day)
+      and total_line_amount is not null /*to handle order with zero payment, 15 Mei 2021 - EDP */
     )
   where rn = 1
 )
 select
-    Company as Company
-    , invoice_currency as invoice_currency
-    , supplier_reference_id as supplier_reference_id
-    , invoice_date as invoice_date
-    , order_id as order_id
-    , order_detail_id as order_detail_id
-    , due_date as due_date
-    , order_detail_name as order_detail_name
-    , spend_category as spend_category
-    , coalesce(quantity,1) as quantity
-    , round(total_line_amount,2) as total_line_amount
-    , round(currency_conversion,2) as currency_conversion
-    , booking_code as booking_code
-    , product_category as product_category
-    , product_provider as product_provider
-    , deposit_flag as deposit_flag
-    , event_name as event_name
-    , null as payment_handling
-    , on_hold_status as on_hold_status
-    , memo as memo
-    , customer_reference_id
+     coalesce(Concat('"',Company,'"'), '""') as Company
+    , coalesce(Concat('"',invoice_currency,'"'), '""') as invoice_currency
+    , coalesce(Concat('"',supplier_reference_id,'"'), '""') as supplier_reference_id
+    , coalesce(Concat('"',safe_cast(invoice_date as string),'"'), '""') as invoice_date
+    , coalesce(Concat('"',safe_cast(order_id as string),'"'), '""') as order_id
+    , coalesce(Concat('"',safe_cast(order_detail_id as string),'"'), '""') as order_detail_id
+    , coalesce(Concat('"',safe_cast(due_date as string),'"'), '""') as due_date
+    , coalesce(Concat('"',order_detail_name,'"'), '""') as order_detail_name
+    , coalesce(Concat('"',spend_category,'"'), '""') as spend_category
+    , coalesce(Concat('"',safe_cast(coalesce(quantity,1) as string),'"'), '""') as quantity
+   /* , coalesce(Concat('"',safe_cast(round(total_line_amount,2) as string),'"'), '""') as total_line_amount -- 08 April 2021, for currency JPY & VND total line amount must be rounding, EDP*/
+    , coalesce(Concat('"',
+        case
+          when invoice_currency in ('JPY', 'VND') then safe_cast(round(total_line_amount,0) as string)
+          else safe_cast(round(total_line_amount,2) as string)
+          end
+          ,'"'), '""') as total_line_amount
+    , coalesce(Concat('"',
+        case
+          when Company <> 'GTN_IDN' then ''
+          else safe_cast(round(currency_conversion,2) as string)
+        end
+      ,'"'), '""') as currency_conversion
+    , coalesce(Concat('"',booking_code,'"'), '""') as booking_code
+    , coalesce(Concat('"',product_category,'"'), '""') as product_category
+    , coalesce(Concat('"',product_provider,'"'), '""') as product_provider
+    , coalesce(Concat('"',deposit_flag,'"'), '""') as deposit_flag
+    , coalesce(Concat('"',event_name,'"'), '""') as event_name
+    , '""' as payment_handling
+    , coalesce(concat('"',on_hold_status,'"'),'""') as on_hold_status
+    , coalesce(concat('"',memo,'"'),'""') as memo
+    , coalesce(concat('"',customer_reference_id,'"'),'""') as customer_reference_id
   from
     tr 
     left join lsw using (order_id, order_detail_id)
